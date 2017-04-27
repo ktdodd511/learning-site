bddbd76d04a4bae7b474c424b3c73748bcea63db
ktdodd511 <katiedodd@Katies-MacBook-Air-2.local>
2017-04-27 10:23:14 -0700

    added view for course steps

--- courses/views.py
+++ courses/views.py
@@ -11,3 +11,7 @@ def course_list(req):
 def course_detail(req, pk):
     course = get_object_or_404(Course, pk=pk)
     return render(req, 'courses/course_detail.html', {'course': course})
+
+def step_detail(req, course_pk, step_pk):
+    step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
+    return render(req, 'courses/step_detail.html', {'step': step})
